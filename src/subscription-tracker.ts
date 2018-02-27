92010c272e27d9cc8d45644731020e814e7cbf34
Kevin Phelps <KevinPhelps11@gmail.com>
2018-02-26 19:36:27 -0600

    Revert "fix: ensure all observers receiver error notifications"
    This reverts commit a916a60cc6bc71a1ee8dc7cbcae27569b50b40d7 because it prevents
    error from being properly reported.

--- src/subscription-tracker.ts
+++ src/subscription-tracker.ts
@@ -5,8 +5,7 @@ export abstract class SubscriptionTracker {
   private subscriptions: Subscription[] = [];
 
   static subscribeAndTrack<T>(this: Observable<T>, tracker: SubscriptionTracker, next?: (value: T) => void, error?: (error: any) => void, complete?: () => void) {
-    // tslint:disable-next-line:no-empty
-    tracker.subscriptions.push(this.subscribe(next, error || (() => { }), complete));
+    tracker.subscriptions.push(this.subscribe(next, error, complete));
   }
 
   protected destroy() {
